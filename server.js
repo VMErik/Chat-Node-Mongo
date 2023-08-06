d7a61e82d2a1c2bde011f34f68f93a98685e8a17
Erik <erik.monroy.an@gmail.com>
2023-08-05 23:09:52 -0600

    Trabajando con tipos de respuestas

--- server.js
+++ server.js
@@ -13,16 +13,25 @@ app.use(router);
 
 // Indicamos nuestra ruta para el metodo get
 router.get('/message', function(req, res) {
+    console.log(req.headers); // Incluye las cabeceras
+    res.header({ "custom-header": "Nuestro valor personalizado" }); // Podemos enviar headers dentro de las respuestas
     res.send('Aqui esta tu lista de mensajes');
 });
 
 // Indicamos nuestra ruta para el metodo post
 router.post('/message', function(req, res) {
-    console.log(req.body); // accedemos a los parametros del body
-    console.log(req.query) // accedemos a los parametros del query
-    res.send('Se ha aregado tu mensaje ' + req.body.text);
+    // Podemos enviar estados (201)
+    res.status(201).send({
+        error: '',
+        body: 'Creado correctamente'
+    });
 });
 
+router.post('/message', function(req, res) {
+    console.log(req.body); // accedemos a los parametros del body
+    console.log(req.query) // accedemos a los parametros del query
+    res.send('Se ha eliminado tu mensaje ' + req.body.text);
+});
 
 app.listen('3000');
 console.log('La aplicacion esta escuchando en http://localhost:3000');
